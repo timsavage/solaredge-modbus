14fb3e2753576bbd369cc7968cb4a91266f7572b
Tim Savage <tim@savage.company>
2024-11-21 02:46:44 +1100

    Add meters...

--- pyse/enums.py
+++ pyse/enums.py
@@ -1,10 +1,17 @@
-from enum import IntEnum
+from enum import IntEnum, IntFlag
 
 
 class InverterDeviceType(IntEnum):
     SinglePhase = 101
     SplitPhase = 102
     ThreePhase = 103
+
+
+class MeterDeviceType(IntEnum):
+    SinglePhase = 201
+    SplitPhase = 202
+    WyeThreePhase = 203
+    DeltaThreePhase = 204
 
 
 class InverterDeviceStatus(IntEnum):
@@ -17,3 +24,20 @@ class InverterDeviceStatus(IntEnum):
     ShuttingDown = 6
     Fault = 7
     Standby = 8
+
+
+class MeterEventFlags(IntFlag):
+    Power_Failure = 0x00000004
+    Under_Voltage = 0x00000008
+    Low_PF = 0x00000010
+    Over_Current = 0x00000020
+    Over_Voltage = 0x00000040
+    Missing_Sensor = 0x00000080
+    Reserved1 = 0x00000100
+    Reserved2 = 0x00000200
+    Reserved3 = 0x00000400
+    Reserved4 = 0x00000800
+    Reserved5 = 0x00001000
+    Reserved6 = 0x00002000
+    Reserved7 = 0x00004000
+    Reserved8 = 0x00008000
--- solaredge.py
+++ solaredge.py
@@ -1,10 +1,16 @@
 from pymodbus.client import ModbusTcpClient
+
+from pyse.meter import MeterCommonModel, MeterDataModel
 from pyse.models import CommonModel, InverterModel
 
 client = ModbusTcpClient("192.168.1.140", port=1502)
 with client:
     common = CommonModel(client)
     print(common)
-    print("")
     inverter = InverterModel(client)
     print(inverter)
+    print("")
+    meter = MeterCommonModel.meter_1(client)
+    print(meter)
+    data = MeterDataModel.meter_1(client)
+    print(data)
