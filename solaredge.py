b1152414139489cd4bd373d05334eb0cd6262d62
Tim Savage <tim@savage.company>
2024-11-20 23:27:16 +1100

    Add simple sample script

--- solaredge.py
+++ solaredge.py
@@ -4,7 +4,7 @@ from pyse.models import CommonModel, InverterModel
 client = ModbusTcpClient("192.168.1.140", port=1502)
 with client:
     common = CommonModel(client)
-    inverter = InverterModel(client)
-
     print(common)
+    print("")
+    inverter = InverterModel(client)
     print(inverter)
