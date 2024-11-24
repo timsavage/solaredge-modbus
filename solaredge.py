70ddf600a29acf93faab8ebc9eb2de9a4d1be126
Tim Savage <tim@savage.company>
2024-11-25 00:26:04 +1100

    Update sample script to match changes to models

--- solaredge.py
+++ solaredge.py
@@ -1,16 +1,26 @@
 from pymodbus.client import ModbusTcpClient
 
-from pyse.meter import MeterCommonModel, MeterDataModel, CommonModel
-from pyse.models import InverterModel
+from pyse import MeterCommonModel, InverterModel, MeterDataModel, CommonModel
 
-client = ModbusTcpClient("192.168.1.140", port=1502, )
+client = ModbusTcpClient(
+    "192.168.1.140",
+    port=1502,
+)
 with client:
     common = CommonModel(client)
+    common.refresh()
     print(common)
+
     inverter = InverterModel(client)
+    inverter.refresh()
     print(inverter)
+
     print("")
+
     meter = MeterCommonModel.meter_1(client)
+    meter.refresh()
     print(meter)
+
     data = MeterDataModel.meter_1(client)
+    data.refresh()
     print(data)
